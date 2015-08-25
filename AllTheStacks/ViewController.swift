971da45cf53a9c27a476b2a09624373ba52c7334
Adam Ahrens <adam.ahrens@smartthings.com>
2015-08-25 13:07:14 -0500

    Initial Commit

---
+++ AllTheStacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllTheStacks
+//
+//  Created by Adam Ahrens on 8/25/15.
+//  Copyright © 2015 Appsbyahrens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
